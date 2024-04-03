f931198a8b2ffe6f13f3367b695587aa04265e58
JC MISA <johncarlomisa399@gmail.com>
2024-04-03 23:50:16 +0800

    remove fullcalendar from app folder

--- configurations/config.php
+++ configurations/config.php
@@ -1,5 +1,10 @@
 <?php
 
+$server = "sql312.infinityfree.com";
+$username = "if0_36297433";
+$password = "mPgNmuqNSbaxcj";
+$dbname = "if0_36297433_sou_db";
+
 $conn = mysqli_connect('localhost','root','','sou_db');
 
 if ($conn->connect_error)
